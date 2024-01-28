178659fe0eaf4bf932435a6e197e3a87758164b4
Rahul Kumar Verma <r.k.vermalnh@gmail.com>
2024-01-28 08:30:20 +0530

    array basic problems

---
+++ src/arrays/ArrayInsertAtEndProb.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class ArrayInsertAtEndProb {
+    public void insertAtEnd(int arr[],int sizeOfArray,int element)
+    {
+        //Your code here
+        arr[sizeOfArray-1] = element;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/arrays/ArrayInsertAtIndex.java
@@ -0,0 +1,31 @@
+package arrays;
+
+public class ArrayInsertAtIndex {
+    private static void insertAtIndex(int arr[],int sizeOfArray,int index,int element)
+    {
+        for(int i=arr.length -1;i>index;i--){
+            arr[i+1] = arr[i];
+        }
+        arr[index] = element;
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = new int [6];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        arr[3]=4;
+        arr[4]=5;
+        int index = 5;
+        int element= 90;
+        insertAtIndex(arr,6,index,element);
+        for(int ele: arr){
+            System.out.println(ele);
+        }
+
+
+
+    }
+}
