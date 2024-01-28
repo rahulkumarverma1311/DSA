69dd99b3294f34d10cb229d7f1be4d8e8ee91851
Rahul Kumar Verma <r.k.vermalnh@gmail.com>
2024-01-28 08:31:47 +0530

    test case completed

--- src/arrays/ArrayInsertAtIndex.java
+++ src/arrays/ArrayInsertAtIndex.java
@@ -3,7 +3,7 @@ package arrays;
 public class ArrayInsertAtIndex {
     private static void insertAtIndex(int arr[],int sizeOfArray,int index,int element)
     {
-        for(int i=arr.length -1;i>index;i--){
+        for(int i=sizeOfArray-2;i>=index;i--){
             arr[i+1] = arr[i];
         }
         arr[index] = element;
