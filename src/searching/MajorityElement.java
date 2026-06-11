66f7b7f7ea8c489e096aca98debac578c36d92fa
Rahul <r.k.vermalnh@gmail.com>
2026-06-11 23:23:12 +0530

    day 1 of 160

--- src/searching/MajorityElement.java
+++ src/searching/MajorityElement.java
@@ -1,16 +1,16 @@
 package searching;
 
 public class MajorityElement {
-    private static int getMajorityElement(int arr[]){
-        int count =0;
-        if(arr.length ==1){
+    private static int getMajorityElement(int arr[]) {
+        int count = 0;
+        if (arr.length == 1) {
             return arr[0];
         }
-        for(int i=0;i< arr.length -1; i++){
-            for (int j = i+1; j< arr.length;j++){
-                if(arr[i] ==  arr[j]){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
                     count++;
-                    if(count > (arr.length / 2)){
+                    if (count > (arr.length / 2)) {
                         return arr[i];
                     }
                 }
@@ -18,9 +18,38 @@ public class MajorityElement {
         }
         return -1;
     }
+
+    private static int majorityElement(int arr[]) {
+        // code here
+            int element = 0;
+            int n = arr.length;
+            int count = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (count == 0) {
+                    count++;
+                    element = arr[i];
+                } else if (element == arr[i]) {
+                    count++;
+                } else {
+                    count--;
+                }
+            }
+            count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == element)
+                    count++;
+            }
+            if (count > (n / 2)) {
+                return element;
+            }
+            return -1;
+    }
+
     public static void main(String[] args) {
 //        int arr[] = {1, 1, 2, 1, 3, 5, 1};
-        int arr[] = {2,13};
+        int arr[] = {2, 13};
         System.out.println(getMajorityElement(arr));
+//        System.out.println(majorityElement(arr));
     }
 }
