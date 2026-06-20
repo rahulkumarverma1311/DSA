6c58a009b0fc936d24f35c6114e41ff31fc19f19
Rahul <r.k.vermalnh@gmail.com>
2026-06-20 12:12:03 +0530

    searching day 2 part 2

---
+++ src/searching/CountOnesInSortedBinaryArray.java
@@ -0,0 +1,27 @@
+package searching;
+
+public class CountOnesInSortedBinaryArray {
+
+    private static int countOnce(int arr[], int n) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if(arr[mid] == 0){
+                low = mid + 1;
+            }else{
+                if(mid ==0 || arr[mid -1] == 0){
+                    return n - mid;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {0,0,1,1,1,1,1};
+        System.out.println(countOnce(arr,arr.length));
+    }
+}
